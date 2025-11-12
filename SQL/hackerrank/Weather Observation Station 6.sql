5695577b21fe1d3f785cab4d1393194a45faad1a
Your Name <iommar@proton.me>
2025-11-12 10:00:31 +0200

    Add HackerRank SQL solution for Weather Observation Station 6

--- SQL/hackerrank/Weather Observation Station 6.sql
+++ SQL/hackerrank/Weather Observation Station 6.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT city
 FROM station 
 WHERE city LIKE 'E%' 
          OR city LIKE 'A%'
-         OR city  LIKE 'I%' 
+         OR city LIKE 'I%' 
          OR city LIKE 'O%'
          OR city LIKE 'U%'
         
