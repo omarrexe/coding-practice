b41ba9f72564aa32734dd32ed62b787905e5220e
Your Name <iommar@proton.me>
2025-11-12 09:38:11 +0200

    solve problem

---
+++ SQL/hackerrank/Weather Observation Station 6.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT city
+FROM station 
+WHERE city LIKE 'E%' 
+         OR city LIKE 'A%'
+         OR city  LIKE 'I%' 
+         OR city LIKE 'O%'
+        or city LIKE 'U%'
+        
+        
