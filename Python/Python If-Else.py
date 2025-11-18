707f19e737ca2002c1f08e0e942f211a32802dfc
Your Name <iommar@proton.me>
2025-11-19 00:55:50 +0200

    solve problem

---
+++ Python/Python If-Else.py
@@ -0,0 +1,10 @@
+n=3
+
+if (n<5 and n>2 and n%2==0):
+    print('Not Weird')
+elif(n>6 and n<20 and n%2==0):
+    print('Weird')
+elif(n>20 and n%2==0):
+    print('Not Weird')
+else:
+    print('Weird')
