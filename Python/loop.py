de3718ece3febb3daa1a36aa3140bb437bc1298c
Your Name <iommar@proton.me>
2025-11-20 12:35:08 +0200

    solve problem

---
+++ Python/loop.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    for nubmer in range(n):
+        print(nubmer ** 2)
+
---
+++ Python/print function.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    n = int(input())
+def myfunction(n):
+    for number in range(n):
+        print(number+1,end="")
+
+
+b=None
+
+def newfunction(b):
+    for nmber in range(b):
+        print(nmber+1,end=" ")
+
+newfunction(10)
+
+myfunction(n)
